e60f14b1a6d898fbd6096ffef7cfb944cba6fd22
johnaoga <johnaoga@gmail.com>
2015-04-12 14:37:05 +0100

    first commit

---
+++ Code_Projet/AppsChalenges.ino
@@ -0,0 +1,145 @@
+int feux_pin  = 3, clock_pin = 2, reset = 4;
+int feux_leds [] = {0,0,0,0,0,0,0,0,0,0,0,0},force [] = {0,0,0,0,0,0,0,0,0,0,0,0}, voie []={0,0,0,0};
+int led,i,j=0,etat=0,teste;
+String val="";
+int dureeV = 1.5 ,dureeJ=0.5 ,dureeR=0.2;
+float tempoLed[] = {1.5,0.5,0.2,1.5,0.5,0.2,1.5,0.5,0.2,1.5,0.5,0.2};
+//--------------------------
+void affiche(int tab[]){ //Fonction d'envoie des données vers les lampes et le port com
+  val="";
+  digitalWrite(reset,LOW);
+  digitalWrite(reset,HIGH);
+    for(led=11; led>=0;led--){
+      digitalWrite(clock_pin,LOW);
+        //delay(2);
+          digitalWrite(feux_pin,tab[led]);
+        //delay(2);
+      digitalWrite(clock_pin,HIGH);
+        //delay(2);
+        val.concat(tab[11-led]);
+    }         
+  Serial.println(val);  //vers port com
+}    
+//-----------------------------
+void change(int posi){ //configuration des etats des lampes
+        feux_leds [posi] = 1 ;
+    if(posi%3>0){
+        feux_leds [posi-1] = 0;
+    }
+    if(posi%3==0){
+        feux_leds [posi+2] = 0;
+    }
+}
+//-----------------------------
+void duree(int posi){ //temporisation de chaque etat
+ // for(i=0;i<100;i++){
+     delay(tempoLed[posi]*1000);
+ //    lecture();
+  //   if(Serial.available()>0){
+  //   break;
+  //   }
+  // }  
+}
+//-----------------------------
+void lecture(){  //Lire le buffer
+  if (Serial.available()>0){
+   teste= Serial.parseInt();
+      if(teste==1){
+          dureeV=Serial.parseInt();
+          dureeJ=Serial.parseInt();
+          dureeR=Serial.parseInt();
+          
+          for(i=0;i<12;i++){
+              switch(i%3){
+                
+              case 0:
+                    tempoLed[i]= dureeV;
+              break;
+              case 1:
+                    tempoLed[i]= dureeJ;
+              break;
+              case 2:
+                    tempoLed[i]= dureeR;
+              break;
+              }
+           }
+      }
+      if (teste==2){
+
+         for(i=0;i<4;i++){
+              voie[i]=Serial.parseInt();
+          }
+         for(i=11;i>=0;i--){
+           force[i]=0;
+         }
+  
+          for(i=11;i>0;i=i-3){
+              if(voie[i/3]==1){
+                  force[i] = 1;
+              }
+              else{
+                  force[i-2] = 1;
+              }
+              
+          }
+          
+      }
+      if(teste==3){
+          for(i=0;i<12;i++){
+                  tempoLed[i]=Serial.parseInt();
+           }
+      
+      }
+  }
+
+}
+//-----------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode( clock_pin,OUTPUT);
+  pinMode( feux_pin,OUTPUT);
+  
+  for(i=0; i<4; i++){
+    feux_leds [1] = 1;
+    feux_leds [4] = 1;
+    feux_leds [7] = 1;
+    feux_leds [10] =1;
+    affiche(feux_leds);
+    
+    delay(1000);
+    feux_leds [1] = 0;
+    feux_leds [4] = 0;
+    feux_leds [7] = 0;
+    feux_leds [10] =0;
+    affiche(feux_leds);
+    delay(1000);
+    }
+    feux_leds [2] = 1;
+    feux_leds [5] = 1;
+    feux_leds [8] = 1;
+    feux_leds [11] =1;
+    led=0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+    etat=etat%12;
+    
+  change(etat)  ;
+  affiche(feux_leds);
+  duree(etat);             //A changer
+  //delay(tempoLed[etat]);
+  lecture();
+  
+  while(teste == 2){
+      lecture();
+      affiche(force);
+      delay(500);
+  }
+  
+  
+      etat++;
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Nasa_apps_challenge_cotonou_eeig
 Code for EILG
+
+#Contenu du dossier
+
+# Description
+Au Bénin, nous observons une mauvaise régularisation des flux de circulations, un mauvais fonctionnement des feux tricolores en place ce qui obligent un policier à devoir se positionner pour faire manuellement la régularisation. Hier l’organisation de la circulation des véhicules dans un carrefour se faisait par l’homme, depuis la naissance des systèmes d'automatisation et de détection les carrefours sont contrôlés par des automates plus ou moins intelligents. 
+D'ou la naissance de l'idée de gestion automatisée et plus efficiente de l'existant.
+Ce projet a pour objectif la conception d'un système de commande en temps réel en utilisant ARDUINO et LABVIEW. Ceci sera illustré par l’automatisation des feux d'un carrefour en gérant les séquences des feux et les détections de véhicules et de piétons. Il s'agit aussi d'une interface de supervision à distance pour le contrôle de l'ensemble du système. Ainsi, le système offre une possibilité de prise en main manuelle par un humain.
+Actuellement, nous avions réalisé une maquette pour un carrefour avec arduino et labView. La fonctionnalité de base au niveau du carrefour marche bien. Cette fonctionnalité est répartie en trois modes : Le mode fonctionnement normal, mode forcé et le mode fonctionnement personnalisé. 
+Lorsque le mode fonctionnement normal est choisi, on peut paramétrer la durée d’allumage du Vert, du Jaune et du Rouge de chaque voie. Ces temps sont identiques pour toutes les voies.
+Lorsque le mode fonctionnement personnalisé est choisi, on peut paramétrer la durée d’allumage des lampes de chaque voie. Ces durées peuvent être différentes pour toutes les lampes du feu tricolore.
+Lorsque le mode forcé est choisie, on peut forcer l’allumage de la lampe verte ou de la lampe rouge sur n’importe quelle voie.
+Ces différentes configurations sont effectuées via une interface de supervision crée avec labVIEW.
+L’état dynamique des feux tricolores est présenté aussi bien sur la maquette que dans l’interface de supervision.
+La communication entre Arduino et LabVIEW  est effectuer pour USB.
+
